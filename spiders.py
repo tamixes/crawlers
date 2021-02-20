c09597a98740a59829d684d86e4bc2f3f31d4160
Tamires Carvalho <tamizs@hotmail.com>
2021-02-20 18:26:10 -0300

    Creates save_as functions

--- spiders.py
+++ spiders.py
@@ -1,6 +1,7 @@
 import requests
 from lxml import html
 
+import csv
 import json
 import logging
 
@@ -24,29 +25,49 @@ class VultrSpider:
         self.html = html.fromstring(request.content)
 
     def get_items(self):
+        """Gets the page items."""
         table = self.html.xpath(VULTR_XPATHS['table'])[0]
         rows = table.xpath(VULTR_XPATHS['rows'])
-        items = []
 
         for row in rows:
             price = row.xpath(VULTR_XPATHS['price'])
-            price = find_term(r'(\D\d\D*\d+\/\w+)', "".join(price))
+            price = find_term(r'(\$+\d+\.?\d?\d*\/\w+)', "".join(price))
 
-            self.items.append([
-                ('storage', row.xpath(VULTR_XPATHS['storage'])[0]),
-                ('cpu', row.xpath(VULTR_XPATHS['cpu'])[0]),
-                ('memory', row.xpath(VULTR_XPATHS['memory'])[0]),
-                ('bandwith', row.xpath(VULTR_XPATHS['bandwith'])[0]),
-                ('price', price)
-            ])
+            items = {
+                'storage': row.xpath(VULTR_XPATHS['storage'])[0],
+                'cpu': row.xpath(VULTR_XPATHS['cpu'])[0],
+                'memory': row.xpath(VULTR_XPATHS['memory'])[0],
+                'bandwith': row.xpath(VULTR_XPATHS['bandwith'])[0],
+                'price': price,
+            }
+            self.items.append(items)
 
     def log_items(self):
+        """Log the items."""
         for item in self.items:
             logger.info('Item %d: %s', (self.items.index(item) + 1),  item)
+
+    def save_as_json(self):
+        """Save items as json file."""
+        logger.info('Saving items as json.')
+
+        with open('items.json', 'w') as items_json:
+            json.dump(self.items, items_json, indent=4)
+
+    def save_as_csv(self):
+        """Save items as csv."""
+        logger.info('Saving items as csv.')
+
+        with open('items.csv', 'w') as items_csv:
+            fieldnames = ['storage', 'cpu', 'memory', 'bandwith', 'price']
+            writer = csv.DictWriter(items_csv, fieldnames)
+            writer.writeheader()
+            for item in self.items:
+                writer.writerow(item)
 
 
 if __name__ == '__main__':
     vultr = VultrSpider()
     vultr.start_request()
     vultr.get_items()
-    vultr.log_items()
+
