027300cb11485d2c997f20ae3ac482f5de486f20
Tamires Carvalho <tamizs@hotmail.com>
2021-02-20 22:01:51 -0300

    Refactor on actions

--- run.py
+++ run.py
@@ -2,10 +2,17 @@ import getopt
 import sys
 
 from spiders import DigitalOceanSpider, VultrSpider
+from utils import (save_csv,
+                   save_json,
+                   log_spider_items)
 
 argument_list = sys.argv[1:]
 short_options = "ho:v"
 long_options = ["print", "save_csv", "save_json"]
+
+digital = DigitalOceanSpider()
+vultr = VultrSpider()
+
 
 try:
     arguments, values = getopt.getopt(
@@ -17,34 +24,16 @@ except getopt.error as err:
 for current_argument, current_value in arguments:
     if current_argument in ("-p", "--print"):
         if('digital' in values):
-            digital = DigitalOceanSpider()
-            digital.start_request()
-            digital.get_items()
-            digital.log_items()
+            log_spider_items(digital)
         if('vultr' in values):
-            vultr = VultrSpider()
-            vultr.start_request()
-            vultr.get_items()
-            vultr.log_items()
+            log_spider_items(vultr)
     if current_argument in ("--save_csv"):
         if('digital' in values):
-            digital = DigitalOceanSpider()
-            digital.start_request()
-            digital.get_items()
-            digital.save_as_csv()
+            save_csv(digital)
         if('vultr' in values):
-            vultr = VultrSpider()
-            vultr.start_request()
-            vultr.get_items()
-            vultr.save_as_csv()
+            save_csv(vultr)
     if current_argument in ("--save_json"):
         if('digital' in values):
-            digital = DigitalOceanSpider()
-            digital.start_request()
-            digital.get_items()
-            digital.save_as_json()
+            save_json(digital)
         if('vultr' in values):
-            vultr = VultrSpider()
-            vultr.start_request()
-            vultr.get_items()
-            vultr.save_as_json()
+            save_json(vultr)
